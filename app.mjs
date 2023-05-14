70570f84e0c901636d14ca0c99c69d0235de23e5
Hafief Maulana Aziz <Hafief.Maulana.Aziz@hashmicro.com>
2023-05-14 15:59:44 +0700

    add npm validator

--- app.mjs
+++ app.mjs
@@ -1,5 +1,7 @@
 import * as readline from 'readline/promises';
 import * as fs from 'fs/promises';
+import validator from 'validator';
+
 
 
 const rl = readline.createInterface({
@@ -7,20 +9,26 @@ const rl = readline.createInterface({
     output : process.stdout,
 })
 
-const nama = await rl.question('Masukan Nama Anda? ');
-const noHP = await rl.question('Masukan No Telephone Anda? ');
-const contact = {nama, noHP};
 
 try {
+    const nama = await rl.question('Masukan Nama Anda? ');
+    if (validator.isAlpha(nama, "en-US",{ignore:' -'}) == false) throw "Nama yang dimasukan tidak sesuai format !!";
+
+    const noHP = await rl.question('Masukan No Telephone Anda? ');
+    if (validator.isMobilePhone(noHP, 'id-ID') == false) throw "No HP yang dimasukan tidak sesuai format !!";
+    
+    const contact = {nama, noHP};
     const file = await fs.readFile('contacts.json', 'utf8');
     const contacts = JSON.parse(file);
+    
     contacts.push(contact);
+    
     const promise = await fs.writeFile("contacts.json", JSON.stringify(contacts), 'utf-8');  
     promise;
+    
+    console.log(`Data dengan Nama: ${nama} \nNo Telephone ${noHP} \nSudah Dimasukan !`);
 } catch (error) {
     console.log(error);
 }
-
-console.log(`Data dengan Nama: ${nama} \nNo Telephone ${noHP} \nSudah Dimasukan !`);
 
 rl.close()
