5964c69f2770fdfda19ba04db3116a8198e02316
Hafief Maulana Aziz <Hafief.Maulana.Aziz@hashmicro.com>
2023-05-18 14:28:09 +0700

    mengabtraksi module

--- app.mjs
+++ app.mjs
@@ -1,34 +1,21 @@
-import * as readline from 'readline/promises';
-import * as fs from 'fs/promises';
+import * as contacts from "./contacts.mjs"
 import validator from 'validator';
 
 
-
-const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout,
-})
-
-
-try {
-    const nama = await rl.question('Masukan Nama Anda? ');
-    if (validator.isAlpha(nama, "en-US",{ignore:' -'}) == false) throw "Nama yang dimasukan tidak sesuai format !!";
-
-    const noHP = await rl.question('Masukan No Telephone Anda? ');
-    if (validator.isMobilePhone(noHP, 'id-ID') == false) throw "No HP yang dimasukan tidak sesuai format !!";
+const main = async () =>{
+    try {
+        const nama = await contacts.tulisPertanyaan('Masukan Nama Anda? ');
+        console.log(nama);
+        if (validator.isAlpha(nama, "en-US",{ignore:' -'}) == false) throw "Nama yang dimasukan tidak sesuai format !!";
     
-    const contact = {nama, noHP};
-    const file = await fs.readFile('contacts.json', 'utf8');
-    const contacts = JSON.parse(file);
+        const noHP = await contacts.tulisPertanyaan('Masukan No Telephone Anda? ');
+        if (validator.isMobilePhone(noHP, 'id-ID') == false) throw "No HP yang dimasukan tidak sesuai format !!";
+
+        contacts.simpanContact(nama, noHP);
+
+    } catch (error) {
+        console.log(error);
+    }
     
-    contacts.push(contact);
-    
-    const promise = await fs.writeFile("contacts.json", JSON.stringify(contacts), 'utf-8');  
-    promise;
-    
-    console.log(`Data dengan Nama: ${nama} \nNo Telephone ${noHP} \nSudah Dimasukan !`);
-} catch (error) {
-    console.log(error);
 }
-
-rl.close()
+main();
