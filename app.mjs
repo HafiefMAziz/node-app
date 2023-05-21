26e06a1a6091e8ed00c506c8f7e8bee7d0394325
Hafief Maulana Aziz <Hafief.Maulana.Aziz@hashmicro.com>
2023-05-21 14:18:09 +0700

    belajar express layout pakai ejs dan supoortnya

--- app.mjs
+++ app.mjs
@@ -1,40 +1,36 @@
-import http from 'http';
-import fs from 'fs';
-// const http = require('http');
+import express  from "express"
+import expressLayouts from "express-ejs-layouts" //ejs support
+const app = express()
+const port = 3000
 
-const port = 3000;
+//mensetting agar menggunakan view engine ejs
+app.set('view engine', 'ejs');
+app.use(expressLayouts); //ejs support
 
-const renderHTML = (path, res) => {
-    fs.readFile(path, (err, data) => {
-        // jika error tampilkan hal 404
-        if (err){
-            res.writeHead(404);
-        // jika berhasil
-        } else {
-            res.write(data);
-        }
-        res.end();
-    })
-}
+app.get('/', (req, res) => {
+    //menampilkan halaman index
+    //otomatis root file views
+    res.render('index', {
+        layout: 'layouts/main-layout', //option dari exprees-ejs-layouts
+        title: 'Halaman Index',
+    });
+})
 
-// membuat server
-http.createServer((req, res) => {
-    // mengirimkan sebagai html
-    res.writeHead(200, {
-    'Content-Type': 'text/html',
-  })
-    const url = req.url;
-    switch (url){
-        case '/about' :
-            renderHTML('./about.html', res);
-            break;
-        case '/contact':
-            renderHTML('./contact.html', res);
-            break;
-        default :
-            renderHTML('./index.html', res);
-            break;
-    }
-}).listen(port, () => {
-    console.log(`Server is listening on ${port}`);
-});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        layout: 'layouts/main-layout',
+        title: 'Halaman About',
+    });
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {
+        layout: 'layouts/main-layout',
+        title: 'Halaman Contact',
+    });
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
